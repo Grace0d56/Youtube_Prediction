643d18cc0868abba90b14ef10389fcbe7ebd122e
Grace0d56 <zehui.wang.grace@outlook.com>
2025-04-30 12:54:06 -0400

    Update streamlit_tryipynb.py

--- streamlit_tryipynb.py
+++ streamlit_tryipynb.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1__GM6qGXHeXSRGTxhiG9W_c9r78V7P36
 """
 
-!pip install streamlit
-
 import streamlit as st
 import numpy as np
 import pandas as pd
